5bc9b9c6fb980424169370e32e02bd26b9774cc0
AnassB1 <79224460+Ananas1Anass@users.noreply.github.com>
2022-07-08 12:50:10 +0000

    MODEL_CNN_RNN_PYTORCH

---
+++ train.py
@@ -0,0 +1,146 @@
+import os
+import ast
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from pytorch_lightning.core.lightning import LightningModule
+from pytorch_lightning import Trainer
+from argparse import ArgumentParser
+from model import SpeechRecognition
+from dataset import Data, collate_fn_padd
+from pytorch_lightning.loggers import TensorBoardLogger
+from pytorch_lightning.callbacks import ModelCheckpoint
+
+
+class SpeechModule(LightningModule):
+
+    def __init__(self, model, args):
+        super(SpeechModule, self).__init__()
+        self.model = model
+        self.criterion = nn.CTCLoss(blank=28, zero_infinity=True)
+        self.args = args
+
+    def forward(self, x, hidden):
+        return self.model(x, hidden)
+
+    def configure_optimizers(self):
+        self.optimizer = optim.AdamW(self.model.parameters(), self.args.learning_rate)
+        self.scheduler = optim.lr_scheduler.ReduceLROnPlateau(
+                                        self.optimizer, mode='min',
+                                        factor=0.50, patience=6)
+        return [self.optimizer], [self.scheduler]
+
+    def step(self, batch):
+        spectrograms, labels, input_lengths, label_lengths = batch 
+        bs = spectrograms.shape[0]
+        hidden = self.model._init_hidden(bs)
+        hn, c0 = hidden[0].to(self.device), hidden[1].to(self.device)
+        output, _ = self(spectrograms, (hn, c0))
+        output = F.log_softmax(output, dim=2)
+        loss = self.criterion(output, labels, input_lengths, label_lengths)
+        return loss
+
+    def training_step(self, batch, batch_idx):
+        loss = self.step(batch)
+        logs = {'loss': loss, 'lr': self.optimizer.param_groups[0]['lr'] }
+        return {'loss': loss, 'log': logs}
+
+    def train_dataloader(self):
+        d_params = Data.parameters
+        d_params.update(self.args.dparams_override)
+        train_dataset = Data(json_path='/home/anass/Documents/SPEECH_PYTORCH/train_corpus.json', **d_params)
+        return DataLoader(dataset=train_dataset,
+                            batch_size=self.args.batch_size,
+                            num_workers=self.args.data_workers,
+                            pin_memory=True,
+                            collate_fn=collate_fn_padd)
+
+    def validation_step(self, batch, batch_idx):
+        loss = self.step(batch)
+        return {'val_loss': loss}
+
+    def validation_epoch_end(self, outputs):
+        avg_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
+        self.scheduler.step(avg_loss)
+        tensorboard_logs = {'val_loss': avg_loss}
+        return {'val_loss': avg_loss, 'log': tensorboard_logs}
+
+    def val_dataloader(self):
+        d_params = Data.parameters
+        d_params.update(self.args.dparams_override)
+        test_dataset = Data(json_path='/home/anass/Documents/SPEECH_PYTORCH/validation_corpus.json', **d_params, valid=True)
+        return DataLoader(dataset=test_dataset,
+                            batch_size=self.args.batch_size,
+                            num_workers=self.args.data_workers,
+                            collate_fn=collate_fn_padd,
+                            pin_memory=True)
+
+
+# def checkpoint_callback(args):
+#     return ModelCheckpoint(
+#         filepath='/home/anass/Documents/SPEECH_PYTORCH/saved_models/',
+#         save_top_k=True,
+#         verbose=True,
+#         monitor='val_loss',
+#         mode='min',
+#         prefix=''
+#     )
+
+def main(args):
+    h_params = SpeechRecognition.hyper_parameters
+    h_params.update(args.hparams_override)
+    model = SpeechRecognition(**h_params)
+
+    if args.load_model_from:
+        speech_module = SpeechModule.load_from_checkpoint(args.load_model_from, model=model, args=args)
+    else:
+        speech_module = SpeechModule(model, args)
+
+    logger = TensorBoardLogger(args.logdir, name='speech_recognition')
+    trainer = Trainer(logger=logger)
+
+    trainer = Trainer(
+        max_epochs=50, gpus=1,
+        num_nodes=1,
+        logger=logger, gradient_clip_val=1.0,
+        val_check_interval=args.valid_every,
+        #checkpoint_callback=checkpoint_callback(args),
+        resume_from_checkpoint=args.resume_from_checkpoint
+    )
+    trainer.fit(speech_module)
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument('-w', '--data_workers', default=0, type=int,
+                        help='n data loading workers, default 0 = main process only')
+    parser.add_argument('-db', '--dist_backend', default='ddp', type=str,
+                        help='which distributed backend to use. defaul ddp')
+
+    parser.add_argument('--valid_every', default=1000, required=False, type=int,
+                        help='valid after every N iteration')
+
+    parser.add_argument('--load_model_from', default=None, required=False, type=str,
+                        help='path to load a pretrain model to continue training')
+    parser.add_argument('--resume_from_checkpoint', default=None, required=False, type=str,
+                        help='check path to resume from')
+    parser.add_argument('--logdir', default='tb_logs', required=False, type=str,
+                        help='path to save logs')
+    
+    parser.add_argument('--batch_size', default=64, type=int, help='size of batch')
+    parser.add_argument('--learning_rate', default=1e-3, type=float, help='learning rate')
+    parser.add_argument('--pct_start', default=0.3, type=float, help='percentage of growth phase in one cycle')
+    parser.add_argument('--div_factor', default=100, type=int, help='div factor for one cycle')
+    parser.add_argument("--hparams_override", default="{}", type=str, required=False,
+		help='override the hyper parameters, should be in form of dict. ie. {"attention_layers": 16 }')
+    parser.add_argument("--dparams_override", default="{}", type=str, required=False,
+		help='override the data parameters, should be in form of dict. ie. {"sample_rate": 8000 }')
+
+    args = parser.parse_args()
+    args.hparams_override = ast.literal_eval(args.hparams_override)
+    args.dparams_override = ast.literal_eval(args.dparams_override)
+
+
+    main(args)
