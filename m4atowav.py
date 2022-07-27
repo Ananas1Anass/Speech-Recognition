9dfcd2301ba78a1bb43d754698162d56fcbf8bf8
AnassB1 <79224460+Ananas1Anass@users.noreply.github.com>
2022-07-27 14:16:38 +0000

    Add files via upload
    convert m4a to wav

---
+++ m4atowav.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 26 20:13:02 2022
+
+@author: anass
+"""
+
+import os
+import argparse
+
+from pydub import AudioSegment
+
+formats_to_convert = ['.m4a']
+
+for (dirpath, dirnames, filenames) in os.walk("/home/anass/Projet_Stage/Database-20220727T140818Z-001/Database"):
+    for filename in filenames:
+        if filename.endswith(tuple(formats_to_convert)):
+
+            filepath = dirpath + '/' + filename
+            (path, file_extension) = os.path.splitext(filepath)
+            file_extension_final = file_extension.replace('.', '')
+            try:
+                track = AudioSegment.from_file(filepath,
+                        file_extension_final)
+                wav_filename = filename.replace(file_extension_final, 'wav')
+                wav_path = dirpath + '/' + wav_filename
+                print('CONVERTING: ' + str(filepath))
+                file_handle = track.export(wav_path, format='wav')
+                os.remove(filepath)
+            except:
+                print("ERROR CONVERTING " + str(filepath))
